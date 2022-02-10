e38386d61e97f6f37fe4c5acef58e2115d58afc7
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-02-10 15:30:56 -0300

    recria o db

--- src/controllers/categories/getCategory.js
+++ src/controllers/categories/getCategory.js
@@ -1,6 +1,7 @@
 const { Category } = require('../../../models');
 
 const getCategories = async (req, res, _next) => {
+    console.log(req.body);
     const category = await Category.findAll();
 
     return res.status(200).json(category);
