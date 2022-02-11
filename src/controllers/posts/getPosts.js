06011e0e5855e7e67095006505f1cd43e6886e69
Henrique Alarcon <henrique.alarcon@hotmail.com>
2022-02-11 12:47:59 -0300

    Termina a implementacao da rota que entrega todos os posts publicados

--- src/controllers/posts/getPosts.js
+++ src/controllers/posts/getPosts.js
@@ -3,9 +3,17 @@ const { BlogPost, Category, User } = require('../../../models');
 const getPosts = async (req, res, _next) => {
     const posts = await BlogPost.findAll({
         include: [
-            { model: { User, as: 'user' } },
-            { model: { Category, as: 'categories' } },
-    ],
+            {
+                model: User,
+                as: 'user',
+                attributes: { exclude: ['password'] },
+            },
+            {
+                    model: Category,
+                    as: 'categories',
+                    through: { attributes: [] },
+            },        
+        ],
     });
     return res.status(200).json(posts);
 };
